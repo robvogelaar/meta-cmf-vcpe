462acee022526e463ca89b7e728524fb36cf10f5
Rob Vogelaar <revogelaar@gmail.com>
2025-06-11 13:21:00 -0700

    minor updates
    Signed-off-by: Rob Vogelaar <revogelaar@gmail.com>

--- gen/vcpe-rdkb.sh
+++ gen/vcpe-rdkb.sh
@@ -12,11 +12,9 @@ imagefile=$input
 
 # Check if input matches SCP URL pattern (user@host:/path)
 if [[ $input =~ ^[^@]+@[^:]+:.+ ]]; then
-    # Create tmp directory if it doesn't exist
     mkdir -p ./tmp
     # Extract filename from path
     filename="${input##*/}"
-    # Download file using scp
     if ! scp "$input" "./tmp/$filename"; then
         echo "SCP download failed"
         exit 1
@@ -34,6 +32,7 @@ imagename="${imagefile##*/}"; imagename="${imagename%.tar.bz2}"
 containername="vcpe"
 profilename="${containername}"
 volumename="${containername}-nvram"
+
 lxc image delete ${imagename} 2> /dev/null
 lxc image import $imagefile --alias ${imagename}
 lxc delete ${containername} -f > /dev/null 2>&1
@@ -44,10 +43,9 @@ name: ${containername}
 description: "${containername}"
 config:
     boot.autostart: "false"
-    user.hostname: "your-desired-hostname"
     security.privileged: "true"
     security.nesting: "true"
-    limits.memory: "512MB"
+    limits.memory: "512MiB"
     limits.memory.swap: "false"
     limits.cpu: "0,1"
 devices:
@@ -55,17 +53,17 @@ devices:
         path: /
         pool: default
         type: disk
-        size: "512MB"
+        size: "512MiB"
+
 EOF
+
 # eth0
-#lxc profile device add ${profilename} eth0 nic nictype=bridged parent=wan name=eth0
 lxc profile device add ${profilename} eth0 nic nictype=bridged parent=wan name=eth0 hwaddr=00:16:3e:20:79:68
 
 # eth1
-#lxc profile device add ${profilename} eth1 nic nictype=bridged parent=lan-p1 name=eth1 vlan=100
 lxc profile device add ${profilename} eth1 nic nictype=bridged parent=lan-p1 name=eth1 vlan=100 hwaddr=00:16:3e:16:5f:7c
-sudo bridge vlan add vid 100 dev lan-p1 self
 
+sudo bridge vlan add vid 100 dev lan-p1 self
 
 lxc profile device add ${profilename} wlan0 nic name=wlan0 nictype=macvlan parent=virt-wlan0
 lxc profile device add ${profilename} wlan1 nic name=wlan1 nictype=macvlan parent=virt-wlan1
@@ -74,10 +72,11 @@ lxc profile device add ${profilename} wlan3 nic name=wlan3 nictype=macvlan paren
 
 # nvram
 if ! lxc storage volume show default $volumename > /dev/null 2>&1; then
-    lxc storage volume create default $volumename size=4MB
+    lxc storage volume create default $volumename size=4MiB
 fi
 
-lxc profile device add $profilename nvram disk pool=default source=$volumename path=/nvram 1>/dev/null
+lxc profile device add $profilename nvram disk pool=default source=$volumename path=/nvram
+
 lxc profile set ${profilename} environment.TZ $(date +%z | awk '{printf("PST8PDT,M3.2.0,M11.1.0")}')
 lxc profile set ${profilename} environment.HOME /home/root
 lxc profile set ${profilename} environment.CONTAINER_NAME ${containername}
